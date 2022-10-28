65f4fb75e120d6e335e7ac1368ff81bb859e8735
Aleksas <97021849+aneniskis@users.noreply.github.com>
2022-10-28 14:36:02 +0300

    Delete client directory

--- client/src/baseUrl.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   baseURL: "https://pctask.herokuapp.com/api/",
-// });
-export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/",
-});
